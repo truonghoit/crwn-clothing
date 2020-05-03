30aa97b53e81d29b323b02f873d3917e41eb736e
truonghosna <truongho@snaglobal.net>
2020-05-03 22:20:48 +0700

    Implement sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils.js';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils.js';
 import './sign-in.style.scss';
 
 class SignIn extends React.Component {
@@ -12,12 +12,17 @@ class SignIn extends React.Component {
       password: '',
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.state({
-      email: '',
-      password: '',
-    });
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: '',});
+    } catch (error) {
+      console.log("Error: ", error);
+    }
   }
   handleChange = (event) => {
     const { name, value } = event.target;
